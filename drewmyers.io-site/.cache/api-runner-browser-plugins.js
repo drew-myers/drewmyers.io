c4e143cb50f4d6f919e654125a40d4aa9b34c152
drew-myers <drewmyers1620@gmail.com>
2019-05-04 14:34:39 -0400

    initialized lerna repo

---
+++ drewmyers.io-site/.cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('/Users/acmyers/dev/drewmyers.io/drewmyers.io-site/node_modules/gatsby-plugin-typography/gatsby-browser.js'),
+      options: {"plugins":[],"pathToConfigModule":"src/utils/typography"},
+    }]

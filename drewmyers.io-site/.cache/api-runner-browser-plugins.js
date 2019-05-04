465aa8190b411c73e8ade9c2123941a9bc6541de
drew-myers <drewmyers1620@gmail.com>
2019-05-04 15:14:22 -0400

    updated pages and removed .cache

--- drewmyers.io-site/.cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/acmyers/dev/drewmyers.io/drewmyers.io-site/node_modules/gatsby-plugin-typography/gatsby-browser.js'),
-      options: {"plugins":[],"pathToConfigModule":"src/utils/typography"},
-    }]
--- drewmyers.io-site/src/pages/index.js
+++ drewmyers.io-site/src/pages/index.js
@@ -3,10 +3,9 @@ import Layout from "../components/layout"
 
 export default () => (
   <Layout>
-    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+    <h1>Hi! Welcome to my personal site!</h1>
     <p>
-      What do I like to do? Lots of course but definitely enjoy building
-      websites.
+      This is mostly currently under construction, but thanks for stopping by lol.
     </p>
   </Layout>
 )

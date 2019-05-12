afc4bfc15b68511f4f3d47e30316f33315ff9bf1
drew-myers <drewmyers1620@gmail.com>
2019-05-12 12:56:08 -0400

    fixed path

--- drewmyers.io-site/gatsby-config.js
+++ drewmyers.io-site/gatsby-config.js
@@ -1,7 +1,7 @@
 const path = require(`path`);
 
 module.exports = {
-  pathPrefix: `/drew-myers/drewmyers.io`,
+  pathPrefix: `/drewmyers.io`,
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,

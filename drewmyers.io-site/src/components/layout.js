cb8b85329de07e6bf51439bf35898be608792764
Drew Myers <drewmyers1620@gmail.com>
2019-07-31 14:37:18 -0400

    stripped down scope of site to just page with contact, resume and picture

--- drewmyers.io-site/src/components/layout.js
+++ drewmyers.io-site/src/components/layout.js
@@ -1,10 +1,12 @@
 import React from "react"
-import Header from "./header"
-
+import Helmet from "react-helmet"
 
 export default ({ children }) => (
   <div>
-    <Header />
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>DrewMyers.io</title>
+    </Helmet>
     <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
       {children}
     </div>
--- drewmyers.io-site/src/pages/index.js
+++ drewmyers.io-site/src/pages/index.js
@@ -2,27 +2,23 @@ import React from "react"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
+import resume from "../pdf/drew-myers-resume.pdf"
 
 export default ({ data }) => (
   <Layout>
     <div style={{ textAlign: "center" }} >
       <Img style={{ borderRadius: '50%' }} fixed={data.file.childImageSharp.fixed} />
     </div>
-    <h1>Hi! Welcome to my personal site!</h1>
-    <p>
-      This is mostly currently under construction, but thanks for stopping by lol.
+    <p style={{ marginTop: '40px' }}>
+      Hi, I'm Drew. I'm an enthusiastic full stack dev with 6+ years of experience.
     </p>
     <p>
-  Lorem ipsum dolor sit amet, mea ne facete scaevola recusabo, eam in tota adhuc movet. Iriure fuisset ullamcorper in nam, vel at exerci eirmod. Ne ius justo epicuri. An nam quaeque intellegat, sed ut aliquid voluptaria.
-
-Te oporteat contentiones vel. Ei eam alii omittantur dissentiunt. Vidit clita prodesset eu vel, ubique fabulas moderatius et quo. Commodo platonem vel te.
-
-Partiendo euripidis philosophia duo ex, omnis ridens accusamus in quo. Eum porro graece ad, iusto quidam indoctum ex duo. Ad usu commodo voluptua, te primis salutandi usu. Ut vel populo delicatissimi.
-
-Ius in minim lobortis, detracto adversarium cu eam, nonumy omnesque verterem ne vis. Sit te noster mandamus indoctum, duo quis vidisse cu, vero saperet facilis ei has. Eu sit constituam signiferumque. Mel ei enim voluptatum. Ex per alii senserit, mei eu assum voluptatibus. Ius alii dicit ut, at eum affert ubique, inani laboramus appellantur ea his.
-
-Quaestio platonem eos in, ex vivendum maluisset eos, eu vero idque clita vel. In fugit noster vituperatoribus usu, an ridens similique nam. Ad dicat fierent pri, hinc eius harum mei ut, autem suscipiantur ex usu. Error melius intellegebat nam cu, vero rebum albucius at pro, per ad case nullam
-  </p>
+      <a href={resume} download>Here's my resume</a>, if you want it
+    </p>
+    <p>
+      If you want to get in contact with me you can <a href="mailto:drew@drewmyers.io">email me</a> or hit me up on
+      <a href="https://www.linkedin.com/in/drewmyerslin/"> LinkedIn</a>.
+    </p>
   </Layout>
 )
 
